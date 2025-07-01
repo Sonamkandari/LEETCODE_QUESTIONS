d787bbd022d777772780aeab547af8538fd84e23
sonam kandari <sonamkandari2004@gmail.com>
2025-07-01 23:06:13 +0530

    string question

---
+++ STRINGS/QUES_3330.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        // StringBuilder hi=new stringBuilder();
+        if(word.length()==1){
+            return 1;
+        }
+
+              int count=0;
+              int result=0;
+           for(int i=0;i<word.length()-1;i++){
+            if(word.charAt(i)!=word.charAt(i+1)){
+               count++;
+            }
+            
+            result=word.length()-count;
+        }
+        return result;
+        
+    }
+}
