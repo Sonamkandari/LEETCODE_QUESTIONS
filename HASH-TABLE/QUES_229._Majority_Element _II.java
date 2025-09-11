75e8b849f421fb13f54b0644900156f07884e352
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2025-09-11 23:53:58 +0530

    Update QUES_229._Majority_Element _II.java

--- HASH-TABLE/QUES_229._Majority_Element _II.java
+++ HASH-TABLE/QUES_229._Majority_Element _II.java
@@ -1,5 +1,8 @@
 // brute force Approach for this question
 // writting a brute force approch currently
+
+//time complexity - O(n^2)
+//space complexity -O(n)
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         //here i created array list  so that i can store my majority elements in it
@@ -28,5 +31,38 @@ class Solution {
 
         }
         return list;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+//better approach for this question
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        //creating a list 
+        List<Integer> list = new ArrayList<>();
+        //creating a HashMap
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        int minimum = n / 3+1;// floor value
+        for (int numbers : nums) {
+            //counting the frequency or count of each element
+            map.put(numbers, map.getOrDefault(numbers, 0) + 1);
+            //checking  the number count is equal to minimum or no
+            if (map.get(numbers) == minimum) {
+                // if the number count is equal to minimum add it to the list
+                list.add(numbers);
+            }
+            // check the size of the list if it is equal to the limit breakthe loop
+            if (list.size() == 2) {
+                break;
+            }
+
+        }
+
+        Collections.sort(list);
+        return list;
+
     }
 }
